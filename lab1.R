96ba1c716959f0c72db3b8f58245ce5c0ec440f6
Goda <davidespinhamarques@gmail.com>
2024-03-04 10:30:32 +0000

    lab1 continuation

--- lab1.R
+++ lab1.R
@@ -1,5 +1,6 @@
 library(arules)
 library(arulesViz)
+library(tidyverse)
 
 data("Groceries", package = "arules")
 Groceries
@@ -51,3 +52,32 @@ inspect(subrules)
 
 prules = subset(rules,subset = (lift > 2 & (rhs %in% "whole milk" | rhs %in% "yogurt")))
 inspect(prules)
+
+df = read.csv("/home/goda/Desktop/tacd/german_credit.csv")
+summary(df)
+df <- df[-c(1)]
+df <- df %>% mutate_if(is.character, as.factor)
+df <- discretizeDF(df,methods = list(
+  duration_in_month=list(method="interval",4,labels=c("short","med-short","med-long","long")),
+  credit_amount=list(method="interval",4,labels=c("small","med-small","med-high","high")),
+  age=list(method="interval",4,labels=c("young adult","adult","senior","golden"))
+),
+default = list(method = "interval"))
+summary(df)
+
+dft = as(df, "transactions")
+itemInfo(dft)
+
+rules = apriori(dft)
+plot(rules)
+rules = apriori(df,parameter = list(confidence =1))
+plot(rules)
+inspect(rules[1:5])
+
+item_dft = itemInfo(dft)
+myItems <- subset(item_dft,variables %in% c("age","personal_status_sex","job","housing","purpose"))$labels
+rules = apriori(df,parameter = list(confidence = 0.6, minlen = 2), appearance = list(both = myItems,default = "none"))
+plot(rules)
+
+
+                
