55a7fb0e511b1f1dc8f798509f0ac6d5e2fb99d7
Goda <davidespinhamarques@gmail.com>
2024-05-13 09:43:33 +0100

    4.2 done

--- lab4.R
+++ lab4.R
@@ -1,5 +1,6 @@
 library(tm)
 library(text2vec)
+library(wordcloud)
 
 
 #1
@@ -63,3 +64,44 @@ sim2(as.matrix(dtm_bin),mq,method = 'cosine')
 sim2(as.matrix(dtm_tf),mq,method = 'cosine')
 #c
 sim2(as.matrix(dtm_tfidf),mq,method = 'cosine')
+
+
+
+###################################4.2###########################
+
+#3
+#a
+reut21578 = system.file("texts", "crude", package = "tm")
+reuters = VCorpus(DirSource(reut21578), readerControl = list(reader = readReut21578XMLasPlain))
+
+data("crude")
+reuters
+r0 = reuters 
+
+#b
+inspect(reuters[[1]])
+meta(reuters[[1]])
+
+#c
+wordcloud(reuters,colors = rainbow(20))
+
+#d
+reuters = tm_map(reuters,stripWhitespace)
+reuters = tm_map(reuters,content_transformer(tolower))
+reuters = tm_map(reuters,removeWords,stopwords("english"))
+reuters = tm_map(reuters,stemDocument)
+reuters = tm_map(reuters,removePunctuation)
+
+#e
+wordcloud(reuters,colors = rainbow(20))
+
+#f
+dtm = DocumentTermMatrix(reuters)
+inspect(dtm[5:10,740:743])
+
+#g
+findFreqTerms(dtm,lowfreq = 10)
+
+#h
+findAssocs(dtm,c("opec"),c(0.8))
+
